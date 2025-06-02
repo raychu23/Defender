fed2d4dc9212bbcd33b21f5016527c1770974371
Raymond Chu <98297631+raychu23@users.noreply.github.com>
2025-06-02 11:24:44 -0500

    Clean push without Library folder

---
+++ TigerSampling/Assets/GameSelector.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSelector : MonoBehaviour {
+
+    public enum GameType { TigerStat, TigerSampling}//, TigerPredict }
+
+    public static GameType curGameType = GameType.TigerSampling;
+    public GameType _curGameType;
+
+	void Start () {
+        curGameType = _curGameType;
+	}
+}
---
+++ TigerSampling/Assets/Scripts/Lobby/CorrectLogo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CorrectLogo : MonoBehaviour {
+
+    public Sprite tigerSamplingLogo;
+
+	// Use this for initialization
+	void Start () {
+        Image thisImage = this.GetComponent<Image>();
+        thisImage.sprite = tigerSamplingLogo;
+     
+	}
+}
